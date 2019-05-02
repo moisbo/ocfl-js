add3a533aadcc5fdd5eb7a480d4ae77c62cae863
Moises Sacal <moisbo@gmail.com>
2019-05-03 09:54:10 +1000

    added starter class, tests

---
+++ test/repository.spec.js
@@ -0,0 +1,68 @@
+const assert = require('assert');
+const path = require('path');
+const fs = require('fs-extra');
+const Repository = require('../lib/repository');
+
+
+function createDirectory(aPath) {
+  if (!fs.existsSync(aPath)) {
+    fs.mkdirSync(aPath);
+  }
+}
+
+describe('repository creation', function () {
+
+  describe('no dir', function () {
+    const repoPath = path.join(process.cwd(), './test-data/ocflX');
+    const repository = new Repository(repoPath);
+    it('should test directory', async function f() {
+      try {
+        const init = await repository.initRepo();
+      } catch (e) {
+        assert.strictEqual(e.code, 'ENOENT')
+      }
+    })
+  });
+
+  describe('no init', function () {
+    const repoPath = path.join(process.cwd(), './test-data/notocfl');
+    const repository = new Repository(repoPath);
+    it('should not initialise directories with files', async function () {
+      try {
+        const init = await repository.initRepo();
+      } catch (e) {
+        assert.strictEqual(e.message, 'can\'t initialise a directory here as there are already files');
+      }
+    });
+  });
+});
+
+const repositoryPath = path.join(process.cwd(), './test-data/ocfl1');
+
+describe('createRepo', function () {
+  const repositoryPath = path.join(process.cwd(), './test-data/ocfl1');
+  const repository = new Repository(repositoryPath);
+  createDirectory(repositoryPath);
+
+  try {
+    it('should test content root', async function () {
+      const init = await repository.initRepo();
+      assert.strictEqual(repository.version, '1.0');
+    });
+    it('should have a namaste', function () {
+      assert.strictEqual(repository.path, repositoryPath);
+    });
+    it('should have a namaste file', function () {
+      //create this test path
+      assert.strictEqual(fs.existsSync(path.join(repositoryPath, '0=ocfl_1.0')), true);
+    });
+  } catch (e) {
+    assert.notStrictEqual(e, null);
+  }
+
+});
+
+after(function () {
+  //TODO: destroy test repoPath
+  fs.removeSync(repositoryPath);
+});
