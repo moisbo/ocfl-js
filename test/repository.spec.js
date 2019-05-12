719c640a91244e9c61987a1e2bfed067d59f3f7e
Peter Sefton <peter.sefton@uts.edu.au>
2019-05-13 08:38:30 +1000

    Now removes empty dirs - fixes #3

--- test/repository.spec.js
+++ test/repository.spec.js
@@ -49,16 +49,12 @@ describe('repository init 2', function () {
   const repository = new Repository(repositoryPath);
   const sourcePath1 = path.join(process.cwd(), './test-data/ocfl-object1-source');
   const sourcePath1_additional_files = sourcePath1 + "_additional_files";
-  const repeatedFileHash = "31bca02094eb78126a517b206a88c73cfa9ec6f704c7030d18212cace820f025f00bf0ea68dbf3f3a5436ca63b53bf7bf80ad8d5de7d8359d0b7fed9dbc3ab99"
-  const file1Hash = "4dff4ea340f0a823f15d3f4f01ab62eae0e5da579ccb851f8db9dfe84c58b2b37b89903a740e1ee172da793a6e79d560e5f7f9bd058a12a280433ed6fa46510a"
- 
-  fs.removeSync(sourcePath1_additional_files);
-  shell.cp("-R", sourcePath1, sourcePath1_additional_files);
-  // Add some identical additional files
+  const repeatedFileHash = "31bca02094eb78126a517b206a88c73cfa9ec6f704c7030d18212cace820f025f00bf0ea68dbf3f3a5436ca63b53bf7bf80ad8d5de7d8359d0b7fed9dbc3ab99";
+  const file1Hash = "4dff4ea340f0a823f15d3f4f01ab62eae0e5da579ccb851f8db9dfe84c58b2b37b89903a740e1ee172da793a6e79d560e5f7f9bd058a12a280433ed6fa46510a";
+  const sepiaPicHash = "577b1610764f4d9d05e82b5efe9b39214806e4c29249b59634699101a2a4679317388e78f7b40134aba8371cc684a9b422a7032d34a6785201051e484805bd59";
+  const sepiaPicPath =  "v1/content/sample/pics/sepia_fence.jpg"
+  const sepiaPicLogicalPath =  "sample/pics/sepia_fence.jpg"
 
-  // Add some new additional files
-  fs.writeFileSync(path.join(sourcePath1_additional_files, "sample", "file1.txt"), "$T)(*SKGJKVJS DFKJs");
-  fs.writeFileSync(path.join(sourcePath1_additional_files, "sample", "file2.txt"), "$T)(*SKGJKdfsfVJS DFKJs");
 
     const ocflVersion = "1.0";
     it('should test content root', async function () {
@@ -128,24 +124,68 @@ describe('repository init 2', function () {
      it('should handle file additions', async function(){
       fs.removeSync(repositoryPath);
       createDirectory(repositoryPath);
-      const init = await repository.initRepo();
-      const id = await repository.add_object_from_dir(sourcePath1, "some_other_id");
-      const new_id = await repository.add_object_from_dir(sourcePath1_additional_files, "some_other_id");
+      fs.removeSync(sourcePath1_additional_files);
+      shell.cp("-R", sourcePath1, sourcePath1_additional_files);
+      // Add some identical additional files
+    
+      // Add some new additional files
+      fs.writeFileSync(path.join(sourcePath1_additional_files, "sample", "file1.txt"), "$T)(*SKGJKVJS DFKJs");
+      fs.writeFileSync(path.join(sourcePath1_additional_files, "sample", "file2.txt"), "$T)(*SKGJKdfsfVJS DFKJs");
+    
+      const 
+      init = await repository.initRepo();
+      const test_id = "id";
+      const id = await repository.add_object_from_dir(sourcePath1, test_id);
+      const new_id = await repository.add_object_from_dir(sourcePath1_additional_files, test_id);
       // We got a UUID as an an ID
-      assert.strictEqual(new_id, "some_other_id");
+      assert.strictEqual(new_id, test_id);
       // Check  that the object is there
       const objectPath  = path.join(repositoryPath, new_id.replace(/(..)/g, "$1/"));
       assert.strictEqual(fs.existsSync(objectPath), true);
       // Check that it's v2
       const object = new OcflObject(objectPath);
       const o = await object.init();
-      //assert.strictEqual(object.contentVersion, "32");
       const inv = await object.getInventory();
+
       assert.strictEqual(object.contentVersion, "v2");
       assert.strictEqual(inv.versions["v2"].state[repeatedFileHash].length, 4);
       assert.strictEqual(inv.versions["v2"].state[repeatedFileHash].indexOf("sample/lots_of_little_files/file_0-copy1.txt") > -1, true);
 
+      // Now delete some stuff 
+      const rm = await fs.remove(path.join(sourcePath1_additional_files, "sample", "pics"));
+      const new_id1 = await repository.add_object_from_dir(sourcePath1_additional_files, test_id);
+      // Re-initialize exsiting object
+      const o1 = await object.init();
+      const inv1 = await object.getInventory();
+      assert.strictEqual(object.contentVersion, "v3");  
+      assert.strictEqual(Object.keys(inv1.manifest).length, 211);
+      assert.strictEqual(inv1.manifest[sepiaPicHash][0], sepiaPicPath);
+      // Sepia pic is v2
+      assert.strictEqual(inv1.versions["v2"].state[sepiaPicHash][0], sepiaPicLogicalPath);
+      // Not in v3
+      assert.strictEqual(inv1.versions["v3"].state[sepiaPicHash], undefined);
+
+      // Now put some stuff back
+      console.log(path.join(sourcePath1, "sample","pics"))
+      shell.cp("-R", path.join(sourcePath1, "sample","pics"), path.join(sourcePath1_additional_files, "sample"));
+      const new_id2 = await repository.add_object_from_dir(sourcePath1_additional_files, test_id);
+      const o2 = await object.init();
+      const inv2 = await object.getInventory();
+      assert.strictEqual(object.contentVersion, "v4");  
+      assert.strictEqual(Object.keys(inv1.manifest).length, 211);
+      assert.strictEqual(inv2.manifest[sepiaPicHash][0], sepiaPicPath);
+      // Sepia pic is v2
+      assert.strictEqual(inv2.versions["v4"].state[sepiaPicHash][0], sepiaPicLogicalPath);
+      // Not in v3
+      assert.strictEqual(inv2.versions["v3"].state[sepiaPicHash], undefined);
+      // No content dirs in V3 or v4
+      assert.strictEqual(fs.existsSync(path.join(object.path,"v3","content")), false) ;
+      assert.strictEqual(fs.existsSync(path.join(object.path,"v4","content")), false);
+      // Tho v2 has one
+      assert.strictEqual(fs.existsSync(path.join(object.path,"v2","content")), true);
      });
+
+
 
      // TODO deal with versions (start by refusing to do a a v2)
 
