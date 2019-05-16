985e6cf0c74bc635a0f0c1421a2441557074b7f2
Moises Sacal <moisbo@gmail.com>
2019-05-16 14:43:58 +1000

    formated code with editorconfig

--- demo.js
+++ demo.js
@@ -4,90 +4,90 @@ const fs = require('fs-extra');
 const OCFLRepository = require('./lib/repository');
 
 // This is an asynchronous library so you need to call using await, or use promises
-async function demo(){
-    
-    const demoRepoPath = path.join("demo", "repo");
-    const demoContentPath = path.join("demo", "content");
-    const demoExportPath = path.join("demo", "export");
-    const dd = await fs.remove(demoExportPath);
-    const demoObjectID1 = "https://example.com/object1";
-    const demoObjectID2 = "https://example.com/object2";
-    // new repository needs to be in an empty directory, so make one
-    const rmdir = await fs.remove(demoRepoPath);
-    if (!await fs.exists("demo")){
-     const demodir = await fs.mkdir("demo");
-    }
-    const dir = await fs.mkdir(demoRepoPath);
-    // Make a new repository and initialise it
-    var repo = await new OCFLRepository(demoRepoPath);
-    var init = await repo.initRepo();
+async function demo() {
 
-    // Make up some content
-    const c = await fs.remove(demoContentPath);
-    const rmc = await fs.mkdir(demoContentPath);
+  const demoRepoPath = path.join("demo", "repo");
+  const demoContentPath = path.join("demo", "content");
+  const demoExportPath = path.join("demo", "export");
+  const dd = await fs.remove(demoExportPath);
+  const demoObjectID1 = "https://example.com/object1";
+  const demoObjectID2 = "https://example.com/object2";
+  // new repository needs to be in an empty directory, so make one
+  const rmdir = await fs.remove(demoRepoPath);
+  if (!await fs.exists("demo")) {
+    const demodir = await fs.mkdir("demo");
+  }
+  const dir = await fs.mkdir(demoRepoPath);
+  // Make a new repository and initialise it
+  var repo = await new OCFLRepository(demoRepoPath);
+  var init = await repo.initRepo();
 
-    // Test file
-    const f1 = await fs.writeFile(path.join(demoContentPath, "file1.txt"), "some content");
-    // Empty directory - won't get added to the repo
-    const subDir = path.join(demoContentPath, "folder");
-    const emp = await fs.mkdir(subDir);
+  // Make up some content
+  const c = await fs.remove(demoContentPath);
+  const rmc = await fs.mkdir(demoContentPath);
 
-    // Add demoContentPath to the repository 
-    const new_object1 = await repo.add_object_from_dir(demoContentPath, demoObjectID1);
-    var inv1 = await new_object1.getInventory();
-    console.log("Head version of object 1", inv1.head);
-    console.log("Object 1 has this many files", Object.keys(inv1.versions[inv1.head].state).length)
+  // Test file
+  const f1 = await fs.writeFile(path.join(demoContentPath, "file1.txt"), "some content");
+  // Empty directory - won't get added to the repo
+  const subDir = path.join(demoContentPath, "folder");
+  const emp = await fs.mkdir(subDir);
 
-    const new_object2 = await repo.add_object_from_dir(demoContentPath, demoObjectID2);
+  // Add demoContentPath to the repository 
+  const new_object1 = await repo.add_object_from_dir(demoContentPath, demoObjectID1);
+  var inv1 = await new_object1.getInventory();
+  console.log("Head version of object 1", inv1.head);
+  console.log("Object 1 has this many files", Object.keys(inv1.versions[inv1.head].state).length)
 
-    var inv1 = await new_object1.getInventory();
+  const new_object2 = await repo.add_object_from_dir(demoContentPath, demoObjectID2);
 
-    console.log("Head version of object 1", inv1.head);
+  var inv1 = await new_object1.getInventory();
+
+  console.log("Head version of object 1", inv1.head);
 
 
-    // Same content as before so this will not be saved in the repository when we add the content
-    const f21 = await fs.writeFile(path.join(demoContentPath, "file2.txt"), "some content");
-    const f31 = await fs.writeFile(path.join(demoContentPath, "file3.txt"), "some other content");
-    // Add something to our empty dir
-    const f3 = await fs.writeFile(path.join(subDir, "file4.txt"), "yet more content");
-    var objects = await repo.objects();
-    console.log("We have this many objects:", objects.length);
-   
-    
-    console.log("Re-add the demo directory to object1");
-    const new_object1v1 = await repo.add_object_from_dir(demoContentPath, demoObjectID1);
-    inv1 = await new_object1.getInventory();
+  // Same content as before so this will not be saved in the repository when we add the content
+  const f21 = await fs.writeFile(path.join(demoContentPath, "file2.txt"), "some content");
+  const f31 = await fs.writeFile(path.join(demoContentPath, "file3.txt"), "some other content");
+  // Add something to our empty dir
+  const f3 = await fs.writeFile(path.join(subDir, "file4.txt"), "yet more content");
+  var objects = await repo.objects();
+  console.log("We have this many objects:", objects.length);
 
-    console.log("Head version of object 1:", inv1.head);
-    console.log("Object 1 has this many files:", Object.keys(inv1.versions[inv1.head].state).length)
-    console.log("Removing file3.txt from the content");
-    const rmf3 = await fs.remove(path.join(demoContentPath, "file3.txt"));
-    const new_object1v3 = await repo.add_object_from_dir(demoContentPath, demoObjectID1);
-    inv1 = await new_object1.getInventory();
-    console.log("Head version of object 1:", inv1.head);
-    console.log("Object 1 has this many files:", Object.keys(inv1.versions[inv1.head].state).length)
 
-    console.log("Exporting all the repository objects ") 
+  console.log("Re-add the demo directory to object1");
+  const new_object1v1 = await repo.add_object_from_dir(demoContentPath, demoObjectID1);
+  inv1 = await new_object1.getInventory();
 
-    var objects = await repo.objects();
+  console.log("Head version of object 1:", inv1.head);
+  console.log("Object 1 has this many files:", Object.keys(inv1.versions[inv1.head].state).length)
+  console.log("Removing file3.txt from the content");
+  const rmf3 = await fs.remove(path.join(demoContentPath, "file3.txt"));
+  const new_object1v3 = await repo.add_object_from_dir(demoContentPath, demoObjectID1);
+  inv1 = await new_object1.getInventory();
+  console.log("Head version of object 1:", inv1.head);
+  console.log("Object 1 has this many files:", Object.keys(inv1.versions[inv1.head].state).length)
 
-    async function exportVersions(o) {
-        const inv = await(o.getInventory());
-        const versionList = Object.keys(inv.versions);
-        const id = inv.id;
-        const exportPromises = versionList.map(async  (v) =>  {
-            const exportVDir = path.join(demoExportPath, o.path.replace(/\//g, ""), v)
-            const e = await fs.mkdirp(exportVDir);
-            return repo.export(id, exportVDir,    {"version": v});
-        });
-        return await Promise.all(exportPromises);
-    }
+  console.log("Exporting all the repository objects ")
 
-    const promises = objects.map((o) => exportVersions(o));
-    await Promise.all(promises);
+  var objects = await repo.objects();
 
-    // List objects
+  async function exportVersions(o) {
+    const inv = await (o.getInventory());
+    const versionList = Object.keys(inv.versions);
+    const id = inv.id;
+    const exportPromises = versionList.map(async (v) => {
+      const exportVDir = path.join(demoExportPath, o.path.replace(/\//g, ""), v)
+      const e = await fs.mkdirp(exportVDir);
+      return repo.export(id, exportVDir, { "version": v });
+    });
+    return await Promise.all(exportPromises);
+  }
+
+  const promises = objects.map((o) => exportVersions(o));
+  await Promise.all(promises);
+
+  // List objects
 
 }
 
-demo().then(()=>{console.log("done")})
+demo().then(() => { console.log("done") })
--- test/repository.spec.js
+++ test/repository.spec.js
@@ -27,7 +27,7 @@ describe('repoInit', function () {
       }
 
     });
-   
+
   });
 
   describe('no init', function () {
@@ -40,7 +40,7 @@ describe('repoInit', function () {
         assert.strictEqual(e.message, 'can\'t initialise a directory here as there are already files');
       }
     });
- 
+
   });
 
 });
@@ -48,202 +48,202 @@ describe('repoInit', function () {
 
 describe('repository init 2', function () {
   const repositoryPath = path.join(process.cwd(), './test-data/ocfl1');
-  
+
   const repository = new Repository(repositoryPath);
   const sourcePath1 = path.join(process.cwd(), './test-data/ocfl-object1-source');
   const sourcePath1_additional_files = sourcePath1 + "_additional_files";
   const repeatedFileHash = "31bca02094eb78126a517b206a88c73cfa9ec6f704c7030d18212cace820f025f00bf0ea68dbf3f3a5436ca63b53bf7bf80ad8d5de7d8359d0b7fed9dbc3ab99";
   const file1Hash = "4dff4ea340f0a823f15d3f4f01ab62eae0e5da579ccb851f8db9dfe84c58b2b37b89903a740e1ee172da793a6e79d560e5f7f9bd058a12a280433ed6fa46510a";
   const sepiaPicHash = "577b1610764f4d9d05e82b5efe9b39214806e4c29249b59634699101a2a4679317388e78f7b40134aba8371cc684a9b422a7032d34a6785201051e484805bd59";
-  const sepiaPicPath =  "v1/content/sample/pics/sepia_fence.jpg"
-  const sepiaPicLogicalPath =  "sample/pics/sepia_fence.jpg"
+  const sepiaPicPath = "v1/content/sample/pics/sepia_fence.jpg"
+  const sepiaPicLogicalPath = "sample/pics/sepia_fence.jpg"
 
 
-    const ocflVersion = "1.0";
-    it('should test content root', async function () {
-      fs.removeSync(repositoryPath);
-      createDirectory(repositoryPath);
-      const init = await repository.initRepo();
+  const ocflVersion = "1.0";
+  it('should test content root', async function () {
+    fs.removeSync(repositoryPath);
+    createDirectory(repositoryPath);
+    const init = await repository.initRepo();
 
-      assert.strictEqual(repository.ocflVersion, ocflVersion);
-    });
+    assert.strictEqual(repository.ocflVersion, ocflVersion);
+  });
 
-    it('should have a namaste', function () {
-      assert.strictEqual(repository.path, repositoryPath);
-    });
-    it('should have a namaste file', function () {
-      //create this test path
-      assert.strictEqual(fs.existsSync(path.join(repositoryPath, '0=ocfl_' + ocflVersion)), true);
-    });
+  it('should have a namaste', function () {
+    assert.strictEqual(repository.path, repositoryPath);
+  });
+  it('should have a namaste file', function () {
+    //create this test path
+    assert.strictEqual(fs.existsSync(path.join(repositoryPath, '0=ocfl_' + ocflVersion)), true);
+  });
 
-    const repository2 = new Repository(repositoryPath);
-    it('should initialise in a directory with an existing namaste file', async function () {
-      const init = await repository2.initRepo();
-      assert.strictEqual(repository2.ocflVersion, ocflVersion)
-    });
+  const repository2 = new Repository(repositoryPath);
+  it('should initialise in a directory with an existing namaste file', async function () {
+    const init = await repository2.initRepo();
+    assert.strictEqual(repository2.ocflVersion, ocflVersion)
+  });
 
-    it('should use your id for a new object if you give it one', async function(){
-      const obj = await repository.add_object_from_dir(sourcePath1, "some_other_id");
-      // We got a UUID as an an ID
-      const inv = await(obj.getInventory());
-      assert.strictEqual(inv.id, "some_other_id");
-      // Check  that the object is there
-      const objectPath  = path.join(repositoryPath, inv.id.replace(/(..)/g, "$1/"));
-      assert.strictEqual(fs.existsSync(objectPath), true);
-     });
-
-
-    it('should make up an ID if you add content', async function(){
-      const obj = await repository.add_object_from_dir(sourcePath1);
-      const inv = await(obj.getInventory());
-      const new_id = inv.id;
-      // We got a UUID as an an ID
-      assert.strictEqual(new_id.length, 36);
-      // Check  that the object is there
-      const objectPath  = path.join(repositoryPath, new_id.replace(/(..)/g, "$1/"));
-      assert.strictEqual(fs.existsSync(objectPath), true);
-     });
-
-     
+  it('should use your id for a new object if you give it one', async function () {
+    const obj = await repository.add_object_from_dir(sourcePath1, "some_other_id");
+    // We got a UUID as an an ID
+    const inv = await (obj.getInventory());
+    assert.strictEqual(inv.id, "some_other_id");
+    // Check  that the object is there
+    const objectPath = path.join(repositoryPath, inv.id.replace(/(..)/g, "$1/"));
+    assert.strictEqual(fs.existsSync(objectPath), true);
+  });
 
 
-     it('should refuse to make an object if there is a faiiled attempt in the deposit dir', async function(){
-      try {
-          const depositDir = await fs.mkdir(path.join(repositoryPath, "deposit", "some_id"));
-          const new_id = await repository.add_object_from_dir(sourcePath1, "some_id");
-      }
-      catch (e) {
-        assert.strictEqual(e.message, 'There is already an object with this ID being deposited or left behind after a crash. Cannot proceed.');
-      }
-     });
-
-     it('Should have three objects in it', async function(){
-      const objects = await repository.objects();
-      assert.strictEqual(objects.length, 3)
-      
-      //TODO - Check Object IDs
-
-     });
+  it('should make up an ID if you add content', async function () {
+    const obj = await repository.add_object_from_dir(sourcePath1);
+    const inv = await (obj.getInventory());
+    const new_id = inv.id;
+    // We got a UUID as an an ID
+    assert.strictEqual(new_id.length, 36);
+    // Check  that the object is there
+    const objectPath = path.join(repositoryPath, new_id.replace(/(..)/g, "$1/"));
+    assert.strictEqual(fs.existsSync(objectPath), true);
+  });
 
 
 
-     it('should handle file additions', async function(){
-      fs.removeSync(repositoryPath);
-      createDirectory(repositoryPath);
-      fs.removeSync(sourcePath1_additional_files);
-      shell.cp("-R", sourcePath1, sourcePath1_additional_files);
-      // Add some identical additional files
-    
-      // Add some new additional files
-      fs.writeFileSync(path.join(sourcePath1_additional_files, "sample", "file1.txt"), "$T)(*SKGJKVJS DFKJs");
-      fs.writeFileSync(path.join(sourcePath1_additional_files, "sample", "file2.txt"), "$T)(*SKGJKdfsfVJS DFKJs");
-    
-      const init = await repository.initRepo();
-      const test_id = "id";
-      const id = await repository.add_object_from_dir(sourcePath1, test_id);
-      const obj = await repository.add_object_from_dir(sourcePath1_additional_files, test_id);
-      const inv3 = await obj.getInventory();
-      const new_id = inv3.id;
-      assert.strictEqual(new_id, test_id);
-      // Check  that the object is there
-      const objectPath  = path.join(repositoryPath, new_id.replace(/(..)/g, "$1/"));
-      assert.strictEqual(fs.existsSync(objectPath), true);
-      // Check that it's v2
-      const object = new OcflObject(objectPath);
-      const o = await object.init();
-      const inv = await object.getInventory();
 
-      assert.strictEqual(object.contentVersion, "v2");
-      assert.strictEqual(inv.versions["v2"].state[repeatedFileHash].length, 4);
-      assert.strictEqual(inv.versions["v2"].state[repeatedFileHash].indexOf("sample/lots_of_little_files/file_0-copy1.txt") > -1, true);
+  it('should refuse to make an object if there is a faiiled attempt in the deposit dir', async function () {
+    try {
+      const depositDir = await fs.mkdir(path.join(repositoryPath, "deposit", "some_id"));
+      const new_id = await repository.add_object_from_dir(sourcePath1, "some_id");
+    }
+    catch (e) {
+      assert.strictEqual(e.message, 'There is already an object with this ID being deposited or left behind after a crash. Cannot proceed.');
+    }
+  });
 
-      // Now delete some stuff 
-      const rm = await fs.remove(path.join(sourcePath1_additional_files, "sample", "pics"));
-      const new_id1 = await repository.add_object_from_dir(sourcePath1_additional_files, test_id);
-      // Re-initialize exsiting object
-      const o1 = await object.init();
-      const inv1 = await object.getInventory();
-      assert.strictEqual(object.contentVersion, "v3");  
-      assert.strictEqual(Object.keys(inv1.manifest).length, 211);
-      assert.strictEqual(inv1.manifest[sepiaPicHash][0], sepiaPicPath);
-      // Sepia pic is v2
-      assert.strictEqual(inv1.versions["v2"].state[sepiaPicHash][0], sepiaPicLogicalPath);
-      // Not in v3
-      assert.strictEqual(inv1.versions["v3"].state[sepiaPicHash], undefined);
+  it('Should have three objects in it', async function () {
+    const objects = await repository.objects();
+    assert.strictEqual(objects.length, 3)
 
-      // Now put some stuff back
-      shell.cp("-R", path.join(sourcePath1, "sample","pics"), path.join(sourcePath1_additional_files, "sample"));
-      const new_id2 = await repository.add_object_from_dir(sourcePath1_additional_files, test_id);
-      const o2 = await object.init();
-      const inv2 = await object.getInventory();
-      assert.strictEqual(object.contentVersion, "v4");  
-      assert.strictEqual(Object.keys(inv1.manifest).length, 211);
-      assert.strictEqual(inv2.manifest[sepiaPicHash][0], sepiaPicPath);
-      // Sepia pic is v2
-      assert.strictEqual(inv2.versions["v4"].state[sepiaPicHash][0], sepiaPicLogicalPath);
-      // Not in v3
-      assert.strictEqual(inv2.versions["v3"].state[sepiaPicHash], undefined);
-      // No content dirs in V3 or v4
-      assert.strictEqual(fs.existsSync(path.join(object.path,"v3","content")), false) ;
-      assert.strictEqual(fs.existsSync(path.join(object.path,"v4","content")), false);
-      // Tho v2 has one
-      assert.strictEqual(fs.existsSync(path.join(object.path,"v2","content")), true);
-     });
+    //TODO - Check Object IDs
 
-     // TODO deal with versions (start by refusing to do a a v2)
-     it('should export', async function(){ 
-        // Assume the it function 'should handle file additions' has run and we have an initialized repo in repository
-        const exportDirV4 = path.join("test-data", "exportv4");
-        const exportDirV5 = path.join("test-data", "exportv5");
-
-        const exportDirV1 = path.join("test-data", "exportv1");
-
-        const rmf1 = await fs.remove(exportDirV1);
-        const rmf4 = await fs.remove(exportDirV4);
-        const rmf5 = await fs.remove(exportDirV5);
+  });
 
 
-        const testId = "id";
-        
-        try {
-          const init = await repository.export(testId, exportDirV4); 
-        } catch (e) {
-          assert.strictEqual(e.message, "Can't export as the directory does not exist.");
-        }
 
-        const fl = await fs.writeFile(exportDirV4, "");
-        try {
-          const init = await repository.export(testId, exportDirV4); 
-        } catch (e) {
-          assert.strictEqual(e.message, "Can't export to an existing file.");
-        }
-        const rmf4a = await fs.remove(exportDirV4);
+  it('should handle file additions', async function () {
+    fs.removeSync(repositoryPath);
+    createDirectory(repositoryPath);
+    fs.removeSync(sourcePath1_additional_files);
+    shell.cp("-R", sourcePath1, sourcePath1_additional_files);
+    // Add some identical additional files
 
-        const new41 = await fs.mkdir(exportDirV4);
-        const xp4 = await repository.export(testId, exportDirV4); 
+    // Add some new additional files
+    fs.writeFileSync(path.join(sourcePath1_additional_files, "sample", "file1.txt"), "$T)(*SKGJKVJS DFKJs");
+    fs.writeFileSync(path.join(sourcePath1_additional_files, "sample", "file2.txt"), "$T)(*SKGJKdfsfVJS DFKJs");
 
-        expect(exportDirV4).to.be.a.directory().and.deep.equal(sourcePath1_additional_files, "Matches the stuff that was imported");
+    const init = await repository.initRepo();
+    const test_id = "id";
+    const id = await repository.add_object_from_dir(sourcePath1, test_id);
+    const obj = await repository.add_object_from_dir(sourcePath1_additional_files, test_id);
+    const inv3 = await obj.getInventory();
+    const new_id = inv3.id;
+    assert.strictEqual(new_id, test_id);
+    // Check  that the object is there
+    const objectPath = path.join(repositoryPath, new_id.replace(/(..)/g, "$1/"));
+    assert.strictEqual(fs.existsSync(objectPath), true);
+    // Check that it's v2
+    const object = new OcflObject(objectPath);
+    const o = await object.init();
+    const inv = await object.getInventory();
+
+    assert.strictEqual(object.contentVersion, "v2");
+    assert.strictEqual(inv.versions["v2"].state[repeatedFileHash].length, 4);
+    assert.strictEqual(inv.versions["v2"].state[repeatedFileHash].indexOf("sample/lots_of_little_files/file_0-copy1.txt") > -1, true);
+
+    // Now delete some stuff 
+    const rm = await fs.remove(path.join(sourcePath1_additional_files, "sample", "pics"));
+    const new_id1 = await repository.add_object_from_dir(sourcePath1_additional_files, test_id);
+    // Re-initialize exsiting object
+    const o1 = await object.init();
+    const inv1 = await object.getInventory();
+    assert.strictEqual(object.contentVersion, "v3");
+    assert.strictEqual(Object.keys(inv1.manifest).length, 211);
+    assert.strictEqual(inv1.manifest[sepiaPicHash][0], sepiaPicPath);
+    // Sepia pic is v2
+    assert.strictEqual(inv1.versions["v2"].state[sepiaPicHash][0], sepiaPicLogicalPath);
+    // Not in v3
+    assert.strictEqual(inv1.versions["v3"].state[sepiaPicHash], undefined);
+
+    // Now put some stuff back
+    shell.cp("-R", path.join(sourcePath1, "sample", "pics"), path.join(sourcePath1_additional_files, "sample"));
+    const new_id2 = await repository.add_object_from_dir(sourcePath1_additional_files, test_id);
+    const o2 = await object.init();
+    const inv2 = await object.getInventory();
+    assert.strictEqual(object.contentVersion, "v4");
+    assert.strictEqual(Object.keys(inv1.manifest).length, 211);
+    assert.strictEqual(inv2.manifest[sepiaPicHash][0], sepiaPicPath);
+    // Sepia pic is v2
+    assert.strictEqual(inv2.versions["v4"].state[sepiaPicHash][0], sepiaPicLogicalPath);
+    // Not in v3
+    assert.strictEqual(inv2.versions["v3"].state[sepiaPicHash], undefined);
+    // No content dirs in V3 or v4
+    assert.strictEqual(fs.existsSync(path.join(object.path, "v3", "content")), false);
+    assert.strictEqual(fs.existsSync(path.join(object.path, "v4", "content")), false);
+    // Tho v2 has one
+    assert.strictEqual(fs.existsSync(path.join(object.path, "v2", "content")), true);
+  });
+
+  // TODO deal with versions (start by refusing to do a a v2)
+  it('should export', async function () {
+    // Assume the it function 'should handle file additions' has run and we have an initialized repo in repository
+    const exportDirV4 = path.join("test-data", "exportv4");
+    const exportDirV5 = path.join("test-data", "exportv5");
+
+    const exportDirV1 = path.join("test-data", "exportv1");
+
+    const rmf1 = await fs.remove(exportDirV1);
+    const rmf4 = await fs.remove(exportDirV4);
+    const rmf5 = await fs.remove(exportDirV5);
 
 
-        try {
-          const init = await repository.export(testId, exportDirV4); 
-        } catch (e) {
-          assert.strictEqual(e.message, "Can't export as the directory has stuff in it.");
-        }
-        //assert.strictEqual(false,true); // exported content is the same as /test-data/sourcePath1_additional_files'
+    const testId = "id";
 
-        const new1 = await fs.mkdir(exportDirV1);
-        const xp1  = await repository.export(testId, exportDirV1, {version: "v1"}); 
-        expect(exportDirV1).to.be.a.directory().and.deep.equal(sourcePath1, "Matches the stuff that was imported");
+    try {
+      const init = await repository.export(testId, exportDirV4);
+    } catch (e) {
+      assert.strictEqual(e.message, "Can't export as the directory does not exist.");
+    }
 
-        const new5 = await fs.mkdir(exportDirV5);
+    const fl = await fs.writeFile(exportDirV4, "");
+    try {
+      const init = await repository.export(testId, exportDirV4);
+    } catch (e) {
+      assert.strictEqual(e.message, "Can't export to an existing file.");
+    }
+    const rmf4a = await fs.remove(exportDirV4);
 
-        try {
-          const init = await repository.export(testId, exportDirV5, {version: "v5"}); 
-        } catch (e) {
-          assert.strictEqual(e.message, "Can't export a version that doesn't exist.");
-        }
-     });
+    const new41 = await fs.mkdir(exportDirV4);
+    const xp4 = await repository.export(testId, exportDirV4);
+
+    expect(exportDirV4).to.be.a.directory().and.deep.equal(sourcePath1_additional_files, "Matches the stuff that was imported");
+
+
+    try {
+      const init = await repository.export(testId, exportDirV4);
+    } catch (e) {
+      assert.strictEqual(e.message, "Can't export as the directory has stuff in it.");
+    }
+    //assert.strictEqual(false,true); // exported content is the same as /test-data/sourcePath1_additional_files'
+
+    const new1 = await fs.mkdir(exportDirV1);
+    const xp1 = await repository.export(testId, exportDirV1, { version: "v1" });
+    expect(exportDirV1).to.be.a.directory().and.deep.equal(sourcePath1, "Matches the stuff that was imported");
+
+    const new5 = await fs.mkdir(exportDirV5);
+
+    try {
+      const init = await repository.export(testId, exportDirV5, { version: "v5" });
+    } catch (e) {
+      assert.strictEqual(e.message, "Can't export a version that doesn't exist.");
+    }
+  });
 
 });
 
